3130b2370180f0cbbf0fcb0f75af7c80d9010870
Ferdinand Ramdhani <ferdinram13@gmail.com>
2024-12-11 04:10:14 +0700

    bubble chat ui

--- src/chattingapp/Message.java
+++ src/chattingapp/Message.java
@@ -7,12 +7,14 @@ public class Message {
     private String sender;
     private Instant timestamp;
     private int messageId;
+    private boolean isDeleted;
 
     public Message(String content, String sender, Instant timestamp, int messageId) {
         this.content = content;
         this.sender = sender;
         this.timestamp = timestamp;
         this.messageId = messageId;
+        this.isDeleted = false;
     }
 
     public String getContent() {
@@ -29,5 +31,13 @@ public class Message {
 
     public int getMessageId() {
         return messageId;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
     }
 }
